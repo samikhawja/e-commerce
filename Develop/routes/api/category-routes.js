2d5b671af93345d54d970d5b8abb1baa07afad99
samikhawja <skhawja11@gmail.com>
2021-10-27 09:51:44 -0700

    offline commits included

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -7,10 +7,10 @@ router.get('/',  async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const catergoryData = await Category.findAll({
+    const categoryData = await Category.findAll({
       include: [Product],
     });
-    res.status(200).json(catergoryData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -44,9 +44,38 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+// router.put('/:id', (req, res) => {
+//   Category.update(req.body, {
+//     where: {
+//       id: req.params.id,
+//     },
+//   })
+//     .then((category) => {
+//       return categoryTag.findAll({ where: { category_id: req.params.id } });
+//     })
+//     .then((categoryTags) => {
+//       const categoryTagIds = categoryTags.map(({ tag_id }) => tag_id);
+//       const newCategoryTags = req.body.tagIds
+//         .filter((tag_id) => !categoryTagIds.includes(tag_id))
+//         .map((tag_id) => {
+//           return {
+//             category_id: req.params.id,
+//             tag_id,
+//           };
+//         });
+//       const categoryTagsToRemove = categoryTags
+//         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+//         .map(({ id }) => id);
+//       return Promise.all([
+//         CategoryTag.destroy({ where: { id: categoryTagsToRemove } }),
+//         CategoryTag.bulkCreate(newCategoryTags),
+//       ]);
+//     })
+//     .then((updatedCategoryTags) => res.json(updatedCategoryTags))
+//     .catch((err) => {
+//       res.status(400).json(err);
+//     });
+// });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,15 +6,42 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [Product],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [Product],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    } res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const locationData = await Tag.create({
+      product_id: req.body.product_id,
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
@@ -23,6 +50,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    } res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
