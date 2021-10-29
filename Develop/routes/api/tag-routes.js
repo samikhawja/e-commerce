2a94b75709504d1fae5616b16efcda76de1679f9
samikhawja <skhawja11@gmail.com>
2021-10-28 21:31:48 -0700

    fixed post for catergory and tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -35,9 +35,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const locationData = await Tag.create({
-      product_id: req.body.product_id,
-    });
+    const locationData = await Tag.create(req.body);
     res.status(200).json(locationData);
   } catch (err) {
     res.status(400).json(err);
